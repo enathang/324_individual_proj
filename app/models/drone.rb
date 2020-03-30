8dbbd92e1cfec02d032564c0fab054019b048643
Nathan <enathang@gmail.com>
2020-03-30 15:42:17 -0500

    Adds testing and drone db seeding

--- app/models/drone.rb
+++ app/models/drone.rb
@@ -1,2 +1,4 @@
 class Drone < ApplicationRecord
+    validates :name, presence: true
+    validates :status, presence: true, :numericality => {:greater_than_or_equal_to => 0}
 end
--- test/models/drone_test.rb
+++ test/models/drone_test.rb
@@ -4,4 +4,32 @@ class DroneTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should not save drone without name" do
+    drone = Drone.new
+    drone::status = 1
+    assert_not drone.save, "Saved the drone without a name"
+  end
+
+
+  test "should not save drone without status" do
+    drone = Drone.new
+    drone::name = "Name"
+    assert_not drone.save, "Saved the drone without a status"
+  end
+
+  test "should not save drone with negative status" do
+    drone = Drone.new
+    drone::name = "Name"
+    drone::status = -1
+    assert_not drone.save, "Saved the drone with a negative status"
+  end
+
+  test "should save drone with name and status" do
+    drone = Drone.new
+    drone::name = "Name"
+    drone::status = 1
+    assert drone.save, "Should save drone with name and status"
+  end
+
 end
